1dbe5f9a1fe40bb16cff5e8dfea8d4c9a67fa73e
Ryu-disini <aldrichryuu@gmail.com>
2026-03-31 13:46:57 +0700

    p

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: "10mb", // Memperbesar limit data yang dikirim
-    },
-  },
-  // Tambahkan ini jika belum ada untuk menangani API Routes
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
+      bodySizeLimit: "10mb",
     },
   },
 };
